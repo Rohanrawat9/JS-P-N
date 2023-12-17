db69cfc0e865d49ed050385eb6decbe065fbbe1a
Rohan <126374291+Rohanrawat9@users.noreply.github.com>
2023-12-17 15:53:38 +0000

    variables config done

---
+++ 01_beginner/01_Variables.js
@@ -0,0 +1,15 @@
+const account_id = 26
+let acounteamil = "Rohan@gmail.com"
+var account_number = 765874767
+account_city = "mumbai"
+
+// we cannot change const values once we define
+
+console.log(account_id)
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([account_id, acounteamil, account_number, account_city])
---
+++ 01_beginner/02_datatypes.js
@@ -0,0 +1,4 @@
+ "use strict"; // treat all js code in newer version
+
+//  alert()   // we are using node.js not browser
+
