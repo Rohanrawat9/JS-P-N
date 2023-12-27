afa3d7d5dc773d7a5d27cfea2317346a9a8cc791
Rohan <126374291+Rohanrawat9@users.noreply.github.com>
2023-12-27 17:55:57 +0000

    strings and Maths

---
+++ 01_beginner/05_strings.js
@@ -0,0 +1,37 @@
+const name = "Rohan"
+const repoCount = 50
+
+// console. log(name + repoCount + " Value");
+
+console. log( `Hello my name is ${name} and my repo count is $
+{repoCount}` );
+
+const gameName = new String('Rohanhq')
+
+// console.log(gameName[0])
+
+// console.log(gameName.__proto__)
+
+// console.log(gameName.toUpperCase())
+
+
+const newStringOne = console. log ( gameName.indexOf('t') ) ;
+
+const newString = console.log(gameName.substring(0, 5))
+console.log(newString)
+
+const anotherString = console.log(gameName.slice(-8, 7))
+console.log(anotherString)
+
+
+const newStringone =" Rohan "
+console. log(newStringone);
+console. log(newStringone.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', "-"))
+
+console.log(url.includes ('sundar') )
+
+console.log(gameName.split("-"))
---
+++ 01_beginner/06_number-maths.js
@@ -0,0 +1,38 @@
+const score = 400
+// console. log(score);
+
+const balance = new Number(100)
+// console. log(balance);
+
+// console. log(balance.toString(). length);
+// console.log(balance.toFixed(2));
+
+
+const other_number = 688.87675
+
+// console.log(other_number.toPrecision(3));
+
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ++++++++++++++++++++++ Maths ++++++++++++++++++++
+
+// console. log (Math);
+// console. log (Math.abs(-4));
+// console. log(Math. round(4.4) ) ;
+// console. log(Math.ceil(4.2));
+// console. log(Math.floor(4.9));
+// console. log(Math.min(4, 3, 6, 8));
+// console. log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+
+const min = 48
+const max = 55
+
+console.log(Math.random()* (max - min + 1) + min);
