04e9ad989501738cf0e69eeba932eff494a7bbb8
Rohan <126374291+Rohanrawat9@users.noreply.github.com>
2024-01-12 04:01:54 +0000

    this in js

---
+++ 03_beginner/03_this-and-Arrow.js
@@ -0,0 +1,13 @@
+const user = {
+    name: "Jack",
+    age: 27,
+
+    welcomeMessage: function(){
+        console.log(`${this.name} is the new person here`);
+    }
+};
+
+// user.welcomeMessage()
+// user.name = "nitin";
+// user.welcomeMessage()
+
